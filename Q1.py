8cdf2ba65df0f45b234815261a1fa8c8c89397d3
Cyan232 <paul_duchesneau@hotmail.com>
2022-12-05 10:14:27 -0500

    spectrum

--- Q1.py
+++ Q1.py
@@ -4,32 +4,34 @@ import wave
 import scipy.io.wavfile as swav
 from Hamming import *
 
-#Paramêtre
-M = 512*8
-H = 256
 
-#fft = np.fft.fft(np.multiply(ham,glock[i*H:(i*H)+M]))[0:np.int32(M/2)]       
-#result[:,i] = np.transpose(np.log(np.abs(np.real(fft))))
+#Paramêtre
+M = 4096
+H = 128
 
 samplerate, glock = swav.read("Glock.wav")
 print("Sample rate : " + str(samplerate) + " hz")
 
-result = np.zeros((np.int32(M/2),np.int32(len(glock)/H)))
-fondamentals = np.zeros(np.int32(len(glock)/H))
+#result = np.zeros((M,np.int32(len(glock)/H)))
+#fondamentals = np.zeros(np.int32(len(glock)/H))
 ham = Hamming(M,0.5)
+
+result = np.zeros((np.int32(M/2),np.int32(len(glock)/H)))
 
 for i in range(len(glock)):
     if((i*H)+M)< len(glock): 
         fft = np.fft.fft(np.multiply(ham,glock[i*H:(i*H)+M]))
-        fft =(np.log(np.abs(np.real(fft))))
-        result[:,i] = np.transpose(fft)[0:np.int32(M/2)]
+        fft =(np.log(np.real(np.abs(fft))))
 
-#fft = np.log(np.abs(np.real(np.fft.fft(glock[0:M])))) 
-#fft = np.log(((np.fft.fft(glock[0:M])**2)))
-#ifft = np.fft.ifft(fft)
+        result[:,i] = fft[0:np.int32(M/2)]
+        #result[:,i] = np.where((fft / np.max(fft)) > .5,fft,0)[0:np.int32(M/2)]
+        #result[:,i] = result[:,i] * np.sum(fft)
 
-plt.imshow(result)
+        #---------TEST Alanyse cepstral--------------------
+        #values = (np.fft.ifft(fft))
+        #values = np.transpose(values)[0:M]
+
+        #result[:,i] = values
+        
+plt.imshow(result, cmap='plasma')
 plt.show()
-
-#plt.plot(ifft)
-#plt.show()
