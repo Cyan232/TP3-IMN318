a4e55d03585928ce597ca6f8860ddac835c9bfb6
Paul Duchesneau <paul_duchesneau@hotmail.com>
2022-12-04 21:00:33 -0500

    spectogram test

--- Q1.py
+++ Q1.py
@@ -5,19 +5,25 @@ import scipy.io.wavfile as swav
 from Hamming import *
 
 #Paramêtre
-M = 512
-H = 128
+M = 512*8
+H = 256
 
+#fft = np.fft.fft(np.multiply(ham,glock[i*H:(i*H)+M]))[0:np.int32(M/2)]       
+#result[:,i] = np.transpose(np.log(np.abs(np.real(fft))))
 
-samplerate, glock = swav.read("Glock.wav")
+samplerate, glock = swav.read("E3 6S.wav")
 print("Sample rate : " + str(samplerate) + " hz")
 
-result = np.zeros((M,len(glock)))
+result = np.zeros(np.int32(M/4),np.int32(len(glock)/H))
+fondamentals = np.zeros(np.int32(len(glock)/H))
 ham = Hamming(M,0.5)
-for i in range(0,len(glock),H):
-    if(i+M)< len(glock): 
-        fft = np.fft.fft(np.multiply(ham,glock[i:i+M])) 
-        result[:,i] = np.log(np.abs(np.real(fft)))
 
-plt.plot(result)
+#fft = np.log(np.abs(np.real(np.fft.fft(glock[0:M])))) 
+fft = np.log(((np.fft.fft(glock[0:M])**2)))
+ifft = np.fft.ifft(fft)
+
+#plt.imshow(ifft)
+#plt.show()
+
+plt.plot(ifft)
 plt.show()
